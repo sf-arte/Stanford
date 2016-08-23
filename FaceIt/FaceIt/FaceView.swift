9ccc2bca1ec73729ee6bfa51f7b91e2cb5956fdd
sf-arte <fujino@arte-inc.co.jp>
2016-08-23 14:04:47 +0900

    lecture 4

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -8,6 +8,8 @@ import UIKit
 class FaceView: UIView {
     
     var scale: CGFloat = 0.90
+    var mouthCurvature: Double = 0.0
+    
     var skullRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2
     }
@@ -51,12 +53,34 @@ class FaceView: UIView {
         let eyeCenter = getEyeCenter(eye: eye)
         return pathForCircleCenteredAtPoint(midPoint: eyeCenter, radius: eyeRadius)
     }
+    
+    private func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadius / Ratios.SkullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.SkullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratios.SkullRadiusToMouthOffset
+        
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth / 2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
+        let cp1 = CGPoint(x: mouthRect.minX - mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        let cp2 = CGPoint(x: mouthRect.maxX - mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 5.0
+        return path
+    }
 
     override func draw(_ rect: CGRect) {
         UIColor.blue().set()
         pathForCircleCenteredAtPoint(midPoint: skullCenter, radius: skullRadius).stroke()
         pathForEye(.Left).stroke()
         pathForEye(.Right).stroke()
+        pathForMouth().stroke()
     }
 
 }

9afd4870c57abcdc7accb7141c8b06b9c7a3d547
sf-arte <fujino@arte-inc.co.jp>
2016-08-24 15:01:14 +0900

    Lecture 5

--- FaceIt/FaceIt/FaceViewController.swift
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -15,10 +15,43 @@ class FaceViewController: UIViewController {
     @IBOutlet weak var faceView: FaceView! {
         didSet {
             faceView.addGestureRecognizer(UIPinchGestureRecognizer(
-                target: faceView, action: #selector(FaceView.changeScale(_:))
+                target: faceView, action: #selector(FaceView.changeScale(recognizer:))
             ))
+            let happierSwipeGestureRecognizer = UISwipeGestureRecognizer(
+                target: self, action: #selector(FaceViewController.increaseHappiness)
+            )
+            happierSwipeGestureRecognizer.direction = .up
+            faceView.addGestureRecognizer(happierSwipeGestureRecognizer)
+            let sadderSwipeGestureRecognizer = UISwipeGestureRecognizer(
+                target: self, action: #selector(FaceViewController.decreaseHappiness)
+            )
+            sadderSwipeGestureRecognizer.direction = .down
+            faceView.addGestureRecognizer(sadderSwipeGestureRecognizer)
+            
             updateUI()
         }
+    }
+    
+    @IBAction func toggleEyes(_ recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .ended {
+            switch expression.eyes {
+            case .Open: expression.eyes = .Closed
+            case .Closed: expression.eyes = .Open
+            case .Squinting: break
+            }
+        }
+        
+    }
+    
+    
+    func increaseHappiness()
+    {
+        expression.mouth = expression.mouth.happierMouth()
+    }
+    
+    func decreaseHappiness()
+    {
+        expression.mouth = expression.mouth.sadderMouth()
     }
     
     private var mouthCurvatures = [FacialExpression.Mouth.Frown: -1.0, .Grin: 0.5, .Smile: 1.0, .Smirk: -0.5, .Neutral: 0.0]
--- memo.md
+++ memo.md
@@ -38,4 +38,6 @@ ctrl+右ドラッグで設定する．ctrl+shift+左クリックでレイヤー�
 #### 画像の描画
 
 ### Lecture 5で扱った内容
-####`@IBDesignable`, `@IBInspectable`
+#### `@IBDesignable`, `@IBInspectable`
+#### Gesture
+`addGestureRecognizer`でコードからハンドラを設定する方法と，`@IBAction`でStoryboardから設定する方法．

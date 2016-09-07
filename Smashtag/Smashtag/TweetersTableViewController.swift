57a79f60594c7d389ea04839d0250380385a7335
sf-arte <fujino@arte-inc.co.jp>
2016-09-07 16:20:53 +0900

    Lecture 11

--- Smashtag/Smashtag/TweetersTableViewController.swift
+++ Smashtag/Smashtag/TweetersTableViewController.swift
@@ -9,20 +9,58 @@ import CoreData
 class TweetersTableViewController: CoreDataTableViewController {
     
     var mention: String? { didSet { updateUI() } }
-    var managedObjectContext : NSManagedObjectContext[ { didSet { updateUI() } }
+    var managedObjectContext : NSManagedObjectContext? { didSet { updateUI() } }
     
     private func updateUI() {
+        fetchedResultsController = nil
+        
+        guard let context = managedObjectContext else { return }
+        guard let search = mention else { return }
+        
+        let request : NSFetchRequest<NSFetchRequestResult> = TwitterUser.fetchRequest()
+        request.predicate = NSPredicate(format: "any tweets.text contains[c] %@ and !screenName beginswith[c] %@", search, "darkside")
+        request.sortDescriptors = [NSSortDescriptor(
+            key: "screenName",
+            ascending: true,
+            selector: #selector(NSString.localizedCaseInsensitiveCompare(_:))
+        )]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
     }
 
-    /*
+    private func tweetCountWithMentionByTwitterUser(user: TwitterUser) -> Int? {
+        var count: Int?
+        guard let search = mention else { fatalError("Never called.") }
+        user.managedObjectContext?.performAndWait {
+            let request: NSFetchRequest<NSFetchRequestResult> = Tweet.fetchRequest()
+            request.predicate = NSPredicate(format: "text contains[c] %@ and tweeter = %@", search, user)
+            count = (try? user.managedObjectContext?.count(for: request)) ?? nil
+        }
+        return count
+    }
+    
+    // Required.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TwitterUserCell", for: indexPath)
 
-        // Configure the cell...
+        if let twitterUser = fetchedResultsController?.object(at: indexPath) as? TwitterUser {
+            var screenName: String?
+            // async
+            twitterUser.managedObjectContext?.performAndWait {
+                screenName = twitterUser.screenName
+            }
+            // UI
+            cell.textLabel?.text = screenName
+            
+            if let count = tweetCountWithMentionByTwitterUser(user: twitterUser) {
+                cell.detailTextLabel?.text = (count == 1) ? "1 tweet" : "\(count) tweets"
+            } else {
+                cell.detailTextLabel?.text = ""
+            }
+        }
 
         return cell
     }
-    */
+
 
 }

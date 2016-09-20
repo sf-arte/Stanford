71a41140c93d2e82bf3178df4b051be85fe9eb7e
sf-arte <fujino@arte-inc.co.jp>
2016-09-20 15:25:37 +0900

    fixed Smashtag

--- Smashtag/Smashtag/Tweet+CoreDataClass.swift
+++ Smashtag/Smashtag/Tweet+CoreDataClass.swift
@@ -10,18 +10,16 @@ import Twitter
 
 public class Tweet: NSManagedObject {
     class func tweetWithTwitterInfo(twitterInfo: Twitter.Tweet, inManagedObjectContext context: NSManagedObjectContext) -> Tweet? {
-        let request : NSFetchRequest<NSFetchRequestResult> = Tweet.fetchRequest()
+        let request : NSFetchRequest<Tweet> = Tweet.fetchRequest()
         request.predicate = NSPredicate(format: "unique = %@", twitterInfo.id)
         
-        if let tweet = (try? context.fetch(request))?.first as? Tweet {
-            return tweet
-        } else if let tweet = NSEntityDescription.insertNewObject(forEntityName: "Tweet", into: context) as? Tweet {
+        return (try? request.execute())?.first ?? {
+            let tweet = Tweet(context: context)
             tweet.unique = twitterInfo.id
             tweet.text = twitterInfo.text
             tweet.posted = twitterInfo.created
             tweet.tweeter = TwitterUser.twitterUserWithTwitterInfo(twitterInfo: twitterInfo.user, inManagedObjectContext: context)
             return tweet
-        }
-        return nil
+        }()
     }
 }
--- Smashtag/Smashtag/TwitterUser+CoreDataClass.swift
+++ Smashtag/Smashtag/TwitterUser+CoreDataClass.swift
@@ -10,15 +10,15 @@ import Twitter
 public class TwitterUser: NSManagedObject {
     class func twitterUserWithTwitterInfo(twitterInfo: Twitter.User, inManagedObjectContext context: NSManagedObjectContext) -> TwitterUser?
     {
-        let request : NSFetchRequest<NSFetchRequestResult> = TwitterUser.fetchRequest()
+        let request : NSFetchRequest<TwitterUser> = TwitterUser.fetchRequest()
         request.predicate = NSPredicate(format: "screenName = %@", twitterInfo.screenName)
-        if let twitterUser = (try? context.fetch(request))?.first as? TwitterUser {
-            return twitterUser
-        } else if let twitterUser = NSEntityDescription.insertNewObject(forEntityName: "TwitterUser", into: context) as? TwitterUser {
+       
+        return (try? request.execute())?.first ?? {
+            let twitterUser = TwitterUser(context: context)
             twitterUser.screenName = twitterInfo.screenName
             twitterUser.name = twitterInfo.name
+            
             return twitterUser
-        }
-        return nil
+        }()
     }
 }
